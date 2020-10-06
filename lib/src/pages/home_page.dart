c651d9618b1a14502b0f930a9f7a6401eb255c89
FideL* <fidelvo@gmail.com>
2020-10-05 19:43:12 -0500

    Elaboración de menu a partir de archivo JSON

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -16,35 +16,48 @@ class HomePage extends StatelessWidget {
 
   Widget _lista(){
 
-    //print(menuProvider.opciones);
-    menuProvider.cargarData();
-
     // Un Future tiene varios estados
     // 1. Cuando se está pidiendo información
     // 2. Cuando se resuelve
     // 3. Cuando da un error
 
-    return ListView(
-      children: _crearListaItems(),
+    return FutureBuilder(
+
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
+
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+
+      },
+
     );
 
   }
 
-  List<Widget> _crearListaItems(){
+  List<Widget> _listaItems( List<dynamic> data ){
 
-    return [
-      ListTile(
-        title: Text('Hola Mundo'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola Mundo'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola Mundo'),
-      ),
-    ];
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.blue,),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue,),
+        onTap: (){
+
+        },
+      );
+
+      opciones..add(widgetTemp)
+              ..add(Divider());
+
+    });
+
+    return opciones;
 
   }
 
